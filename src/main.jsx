ecbe721cd949dc9c447f71fd0d2bfafbe0b7d447
prajyotpawaratrevergon <143385605+prajyotpawaratrevergon@users.noreply.github.com>
2024-08-06 10:38:49 +0530

    config : firebase config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import bgVideo from '../src/assets/bg_video.mp4'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <video src={bgVideo} autoPlay loop muted />
     <App />
   </React.StrictMode>,
 )
